4cd94de9563adc7fd68a51442b82459c20a66f6f
Michael Toback <mtoback@vmware.com>
2017-02-20 17:59:44 -0800

    model working

--- driving_nn.py
+++ driving_nn.py
@@ -1,15 +1,21 @@
-import numpy as np
+import argparse
 import csv
 import cv2
+from datetime import datetime
+import keras
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, BatchNormalization
 from keras.layers.core import Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-from  keras.regularizers import l1
-import cv2
+from keras.regularizers import l2
+import numpy as np
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+from scipy.misc import imresize
+import sys
+import time
 '''
 Notes from walkthrough:
 Goal is to drive a car around a track to generate training data 
@@ -46,15 +52,14 @@ def process_image(img):
     img = cv2.GaussianBlur(img,(5, 5),0)
     return img
 
-
-def read_data(root_dir):
+def read_data(root_dir, p_correction, p_threshold, p_filter):
     car_images = []
     steering_angles = []
     csv_file = "./" + root_dir + "/driving_log.csv"
     # create adjusted steering measurements for the side camera images
-    correction = 0.19 # this is a parameter to tune, increased from initial 0.2
-    threshold = 0.985 # only keep 10% of the steering values under +/- 0.15
-    filter = 0.15 # filter value to remove middle values
+    correction = p_correction # amount to add to/subtract from right/left steer
+    threshold = p_threshold # only keep higher % of the steering values under +/- some threshold (so 99% means keep 1%)
+    filter = p_filter # filter value to remove middle values
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -100,13 +105,14 @@ def read_data(root_dir):
 
 def simple_model():
     model = Sequential()
-    model.add(Flatten(input_shape=(160,320,3)))
+    model.add(BatchNormalization(input_shape=(160,320,3), axis=1)) # lambda has issues with unicode, use BatchNormalization instead
+    model.add(Flatten())
     model.add(Dense(1))
     return model
 
 def simple_model_with_normalization_and_cropping():
     model = Sequential()
-    model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
+    model.add(BatchNormalization(input_shape=(160,320,3), axis=1)) # lambda has issues with unicode, use BatchNormalization instead
     #crop the top 70 px and bottom 25px to eliminate parts we don't care about
     model.add(Cropping2D(cropping=((70,25),(0,0))))
     model.add(Flatten())
@@ -118,7 +124,22 @@ def simple_convolutional_model(p_dropout):
     model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
     #crop the top 70 px and bottom 25px to eliminate parts we don't care about
     model.add(Cropping2D(cropping=((70,25),(0,0))))
-    model.add(Convolution2D(3,1,1, activation='relu')) # color filter
+    model.add(Convolution2D(24,5,5, activation='relu')) # color filter
+    model.add(MaxPooling2D(pool_size=(2,2))) # 2,2 causes the model to fail on negative dimension value
+    model.add(Dropout(p_dropout))
+    model.add(Convolution2D(36,5,5, activation='relu')) # color filter
+    model.add(MaxPooling2D(pool_size=(2,2))) # 2,2 causes the model to fail on negative dimension value
+    model.add(Dropout(p_dropout))
+    model.add(Flatten())
+    model.add(Dense(1))
+    return model
+
+def simple_convolutional_model2(p_dropout):
+    model = Sequential()
+    model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
+    #crop the top 70 px and bottom 25px to eliminate parts we don't care about
+    model.add(Cropping2D(cropping=((70,25),(0,0))))
+    model.add(Convolution2D(24,5,5, activation='relu')) # color filter
     model.add(MaxPooling2D(pool_size=(2,2))) # 2,2 causes the model to fail on negative dimension value
     model.add(Dropout(p_dropout))
     model.add(Flatten())
@@ -134,11 +155,24 @@ def simple_mixed_model(p_dropout):
     model.add(MaxPooling2D(pool_size=(2,2))) # 2,2 causes the model to fail on negative dimension value
     model.add(Dropout(p_dropout))
     model.add(Flatten())
-    model.add(Dense(120))
+    model.add(Dense(100))
     model.add(Dense(1))
     return model
 
-def simple_mixed_model2(p_dropout):
+def simple_mixed_model2(p_dropout, p_regularize):
+    model = Sequential()
+    model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
+    #crop the top 70 px and bottom 25px to eliminate parts we don't care about
+    model.add(Cropping2D(cropping=((70,25),(0,0))))
+    model.add(Convolution2D(3,1,1, activation='relu')) # color filter
+    model.add(MaxPooling2D(pool_size=(2,2))) # 2,2 causes the model to fail on negative dimension value
+    model.add(Dropout(p_dropout))
+    model.add(Flatten())
+    model.add(Dense(120, W_regularizer=l2(p_regularize)))
+    model.add(Dense(1))
+    return model
+
+def simple_mixed_model3(p_dropout):
     model = Sequential()
     model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
     #crop the top 70 px and bottom 25px to eliminate parts we don't care about
@@ -150,7 +184,7 @@ def simple_mixed_model2(p_dropout):
     model.add(MaxPooling2D())
     model.add(Dropout(p_dropout))
     model.add(Flatten())
-    model.add(Dense(120))
+    model.add(Dense(100))
     model.add(Dense(1))
     return model
 
@@ -174,21 +208,18 @@ def leNet(p_dropout):
 def nvidia_model(p_dropout, p_regularize):
     # devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/
     model = Sequential()
-    model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
+    model.add(BatchNormalization(input_shape=(160,320,3), axis=1)) # lambda has issues with unicode, use BatchNormalization instead
+    #model.add(Lambda(lambda x: x/255.0 - 0.5,input_shape=(160,320,3)))
     #crop the top 70 px and bottom 25px to eliminate parts we don't care about
-    model.add(Cropping2D(cropping=((70,25),(0,0))))
-    model.add(Convolution2D(3,1,1, activation='relu', W_regularizer=l1(p_regularize))) # color filter
-    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu', W_regularizer=l1(p_regularize)))
-    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu', W_regularizer=l1(p_regularize)))
-    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu', W_regularizer=l1(p_regularize)))
-    model.add(Convolution2D(64,3,3, activation='relu', W_regularizer=l1(p_regularize)))
-    model.add(MaxPooling2D(pool_size=(1,2))) # 2,2 causes the model to fail on negative dimension value
-    model.add(Dropout(p_dropout))
-    model.add(Convolution2D(64,3,3, activation='relu', W_regularizer=l1(p_regularize)))
-    model.add(MaxPooling2D(pool_size=(1,2)))
-    model.add(Dropout(p_dropout))
+    model.add(Cropping2D(cropping=((35,12),(0,0))))
+    #model.add(Convolution2D(3,1,1, activation='relu')) # color filter
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
     model.add(Flatten())
-    model.add(Dense(100, W_regularizer=l1(p_regularize)))
+    model.add(Dense(100))
     model.add(Dense(50))
     model.add(Dense(10))
     model.add(Dense(1))
@@ -204,37 +235,226 @@ def generator(x, y, batch_size=32):
             y_train = np.array(batch_y)
             yield (X_train, y_train)
 
-
+model_list = ['simple_model','simple_model_with_normalization_and_cropping',
+                              'simple_convolutional_model',
+                              'simple_convolutional_model2',
+                              'simple_mixed_model',
+                              'simple_mixed_model2',
+                              'simple_mixed_model3',
+                              'leNet','nvidia_model']
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
-def main():
-    p_dropout = 0.5
-    p_regularize = 0.001
-    X_train, y_train = shuffle(read_data("IMG_TRAIN4"))
-    shape_x = X_train.shape
-    shape_y = y_train.shape
+def show_dist(directory, y_train, p_correction, p_threshold, p_filter):
     n, bins, patches = plt.hist(np.asarray(y_train*100), 50, normed=True)
     
     plt.xlabel('steering')
     plt.ylabel('magniture')
-    plt.title(r'Adjusted steering values')
+    plt.title('Adjusted steering values for %s, corr=%03f,\nthresh=%03f, filt=%03f' % (directory, p_correction, p_threshold, p_filter))
     plt.grid(True)
-    
     plt.show()
+    
+def parser_error(p, msg):
+    print(msg)
+    p.print_usage()
+    sys.exit(-1)
+
+def show_history(history_object):
+    
+    print(history_object.history.keys())    
+    ### plot the training and validation loss for each epoch
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('images=%s, model=%s, drop=%05f,\nreg=%05f,lr=%05f' % 
+              (image_dir, model_name, p_dropout, p_regularize, learning_rate))
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Remote Driving Deep Learning')
+    parser.add_argument(
+        '--model',
+        required=True,
+        type=str,
+        help='names of model to train with'
+    )
+    parser.add_argument(
+        '--images',
+        required=True,
+        type=str,
+        help='folder in this directory containing the training images'
+    )
+    parser.add_argument(
+        '--show_dist',
+        type=bool,
+        default=False,
+        help='show distribution of training data'
+    )
+    parser.add_argument(
+        '--show_hist',
+        type=bool,
+        default=False,
+        help='show training thread of training data'
+    )
+    parser.add_argument(
+        '--dropout_rate',
+        required=True,
+        type=float,
+        help='dropout rate to be applied to model. Should be between 0 and 1.0'
+    )
+    parser.add_argument(
+        '--regularize',
+        required=True,
+        type=float,
+        help='regularization constant. Should be a positive, probably small (< 0.01) value'
+    )
+    parser.add_argument(
+        '--learning_rate',
+        required=True,
+        type=float,
+        help='learning rate constant for model. Should be a positive, probably small (< 0.01) value'
+    )
+    parser.add_argument(
+        '--n_epochs',
+        default=5,
+        type=int,
+        help='number of epochs to train the model. Should be a positive value'
+    )
+    parser.add_argument(
+        '--correction',
+        required=True,
+        type=float,
+        help='plus/minus value be applied to right/left steering values. Should be between 0 and 1.0'
+    )
+    parser.add_argument(
+        '--threshold',
+        required=True,
+        type=float,
+        help='percent values of middle steering values to be dropped'
+    )
+
+    parser.add_argument(
+        '--filter',
+        required=True,
+        type=float,
+        help='band of values for decimation of data be applied to center values. Should be between 0 and 1.0'
+    )
+
+    args = parser.parse_args()
+    if args.images is None or len(args.images)<1:
+        parser_error(parser,"need to specify images directory")
+    if not os.path.exists(args.images):
+        parser_error(parser,"images directory does not exist: %s" % args.images)
+    
+    if args.model is None or len(args.model)<1:
+        parser_error(parser, "need to specify model to use")
+
+    if args.model not in model_list:
+        parser_error(parser, "model %s needs to be one of %s" %(args.model, model_list))
+        
+    
+    if args.dropout_rate >= 1.0 or args.dropout_rate < 0.0:
+        parser_error(parser, "dropout rate needs to be value between 0.0 and 1.0, 1.0 meaning no dropout")
+    
+    if args.regularize >= 1.0 or args.regularize < 0.0:
+        parser_error(parser, "regularization parameter needs to be value between 0.0 and 1.0, 0.0 meaning no regularization")
+
+    if (args.learning_rate >= 1.0) or (args.learning_rate <= 0.0):
+        parser_error(parser, "learning rate needs to be value between 0.0 and 1.0, 0.0 meaning no learning")
+
+    if args.n_epochs >= 1000 or args.learning_rate <= 0:
+        parser_error(parser, "number of epochs needs to be positive value between 1 and 1000")
+    if args.correction >= 1.0 or args.correction < 0.0:
+        parser_error(parser, "correction needs to be value between 0.0 and 1.0, 1.0 meaning no dropout")
+    if args.threshold >= 1.0 or args.threshold < 0.0:
+        parser_error(parser, "threshold rate needs to be value between 0.0 and 1.0, 1.0 meaning no dropout")
+    if args.filter >= 1.0 or args.filter < 0.0:
+        parser_error(parser, "filter rate needs to be value between 0.0 and 1.0, 1.0 meaning no dropout")
+    
+    return {"images": args.images,
+            "dropout": args.dropout_rate,
+            "regularize": args.regularize,
+            "learning_rate": args.learning_rate,
+            "n_epochs": args.n_epochs,
+            "model": args.model,
+            "show_dist": args.show_dist,
+            "show_hist": args.show_hist,
+            "correction": args.correction,
+            "threshold": args.threshold,
+            "filter": args.filter}
+import pickle
+
+def main():
+    params = get_args()
+    p_dropout = params["dropout"]
+    p_regularize = params["regularize"]
+    image_dir = params["images"]
+    learning_rate = params["learning_rate"]
+    n_epochs = params['n_epochs']
+    model_name = params["model"]
+    p_correction = params["correction"]
+    p_threshold = params["threshold"]
+    p_filter = params["filter"]
+    X_train, y_train = shuffle(read_data(image_dir, p_correction, p_threshold, p_filter))
+    if params["show_dist"]:
+        show_dist(image_dir, y_train,p_correction, p_threshold, p_filter)
+    
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
     train_generator = generator(X_train, y_train, batch_size=32)
     validation_generator = generator(X_val, y_val, batch_size=32)
-    # use letNet model
-    model = nvidia_model(p_dropout, p_regularize)
+    
+    model = None
+    if model_name == "simple_model":
+        model = simple_model()
+    elif model_name == "simple_model_with_normalization_and_cropping":
+        model = simple_model_with_normalization_and_cropping()
+    elif model_name == "simple_convolutional_model":
+        model = simple_convolutional_model(p_dropout)
+    elif model_name == "simple_convolutional_model2":
+        model = simple_convolutional_model2(p_dropout)
+    elif model_name == 'simple_mixed_model':
+        model = simple_mixed_model(p_dropout)
+    elif model_name == 'simple_mixed_model2':
+        model = simple_mixed_model2(p_dropout, p_regularize)
+    elif model_name == 'simple_mixed_model3':
+        model = simple_mixed_model3(p_dropout)
+    elif model_name == 'leNet':
+        model = leNet(p_dropout)
+    elif model_name == "nvidia_model":
+        model = nvidia_model(p_dropout, p_regularize)
+    else:
+        print("model method not found: %s" % model_name)
+        sys.exit(-1)
+    
     # use of optimizers, see https://keras.io/optimizers/
     # according to one student, a learning rate of 0.001 yields
     # smoother turns, where 0.0001 yields sharper turns if your
     # car isn't turning far enough
-    model.compile(optimizer='adam', loss='mse', lr=0.001)
-    model.fit_generator(train_generator, samples_per_epoch=len(X_train), 
+    model.compile(optimizer='adam', loss='mse', lr=learning_rate)
+    '''
+    Setting model.fit(verbose = 1) will
+
+        output a progress bar in the terminal as the model trains.
+        output the loss metric on the training set as the model trains.
+        output the loss on the training and validation sets after each epoch.
+    '''
+    history_object = model.fit_generator(train_generator, samples_per_epoch=len(X_train), 
             validation_data=validation_generator, 
-            nb_val_samples=len(X_val), nb_epoch=4)
+            nb_val_samples=len(X_val), nb_epoch=n_epochs, verbose=1)
     model.save('model.h5')
+    if params["show_hist"]:
+        print(history_object.history.keys())
+        
+        ### plot the training and validation loss for each epoch
+        plt.plot(history_object.history['loss'])
+        plt.plot(history_object.history['val_loss'])
+        plt.title('images=%s, model=%s, drop=%05f,\nreg=%05f,lr=%05f' % 
+                  (image_dir, model_name, p_dropout, p_regularize, learning_rate))
+        plt.ylabel('mean squared error loss')
+        plt.xlabel('epoch')
+        plt.legend(['training set', 'validation set'], loc='upper right')
+        plt.show()
 if __name__ == "__main__":
     main()
--- writeup_template.md
+++ writeup_template.md
@@ -70,60 +70,93 @@ The model used an adam optimizer, so the learning rate was not tuned manually (m
 
 ####4. Appropriate training data
 
-Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road ... 
+Initially, I used the sample data with some decimation to take care of too many zero steering values.
 
-For details about how I created the training data, see the next section. 
+My own training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road using a joystick for about 3 laps.
+
 
 ###Model Architecture and Training Strategy
 
 ####1. Solution Design Approach
 
-The overall strategy for deriving a model architecture was to ...
+The overall strategy for deriving a model architecture was to slowly move up the "ladder" of model complexity. We started with a mse error loss and an adam optimizer with a learning rate of 0.001. 
 
-My first step was to use a convolution neural network model similar to the ... I thought this model might be appropriate because ...
+But... before I did that I needed to clean up the data. The data was normalized using batch normalization (the lambda approach did not work, because of encoding errors). A test run without normalization on the simple model produced a mse on the order of 10^6, where with normalization it dropped to around 10.
+
+I also did some filtering of the data which reduced the amount of straight ahead data. Otherwise the amount of zero steering data would so overwhelm the model that it would just go straight all the time. The result was about 13000 data points
 
 In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting. 
 
-To combat the overfitting, I modified the model so that ...
+To combat the overfitting, I modified the convolutional models so that so that it had a dropout value associated with each convolution layer. I varied these values as I got model results as I will describe below.
 
-Then I ... 
+![sample data distribution][201702201042.png]
 
-The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I ....
+I started with a simple model consisting of nothing more than a  Normalization Step, flatten, then an output layer of Dense(1). The results were predictably bad. The MSE was around 6.0 for the validation data after 3 epochs and the car went about 10 feet and drove off the road. Reducing the learning rate by a factor of 10 made it even worse; the validation MSE was constant around 20.
+
+![simple model training][201702201109.png]
+
+Next, I tried adding cropping to the above model. That elminated any changes to the sky from the analysis and hopefully will improve the model response. In fact, the mse dropped to about 1.6 after 3 epochs and continued to drop to 0.86 after 5 epochs. Car just drove right off the road, however.
+
+![simple model with cropping training][201702201147.png]
+
+Next we tried a simple convolutional model; using 24 5x5 filters (the first layer of the nvidia model), relu activation, max pooling with a 2x2 filter and dropout of 0.7. The learning rate initially is set to 0.001.
+
+![simple convolutional model][201702201203.png]
+
+With this model I achieved a validation mse of about 0.02 fairly constant over 5 epochs. The car drove successfully around the track with no problems except for trying to ride the edge just after the bridge. But it drives contiunously. 
+
+I saved this as model.h5.sample_simple_conv
+
+![simple convolutional model][sample_data_simple_conv_out.mp4]
+
+To see if adding another layer would improve things, I added the second convolutional layer from the nvidia model (36 5x5 filters) with the same parameters as above. Did not seem to have a noticeable effect on the mse, but the issue when it came off the bridge was lessened; it only rode on the side of the ledge after bridge once, as opposed to three or four times with the single layer.
+
+I saved this as model.h5.sample_simple_conv2
+
+![simple convolutional model two layers][201702201256.png]
+
+Next, instead of two layers of convolution, we used a single layer and tried inserting a Dense(100) layer between the Flatten and output Dense(1) layer. This is paralleling the Nvidia model, as this is the first dense layer it has This seemed to perform worse than the first two convolutional models, as it stabilized with an mse of about 0.03. Although the ride was much smoother, once it left the bridge the car seemed to "live" on the edge of the road.
+
+![mixed model one conv one dense][201702201319.png]
+
+Before we give up on this, let's see what happens if we add an l2 regularization value to the new dense layer. We set the l2 value to 0.001. The result was a validation mse decreasing, but barely. And the car crashed into the front of the bridge. So this is definitely worse
+
+![mixed model one conv one dense with regularization][201702201342.png]
+
+Now let's go back to the best model, which I think is the two layer convolutional model and see if better data will help. We recorded our own data with recovery data:
+
+![simple convolutional model two layers][201702201402.png]
+
+The result was a worse value than before and the mse for the validation data seemed to head downward but bounced around. It drove off the curve before the bridge.
+
+![simple convolutional model two layers][201702201429.png]
+![simple convolutional model two layers][201702201445.png]
+
+
+The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I reduced the filter to 0.02 and the threshold to 96% and dropout rate to 0.5. 98% worked but resulted in a lot of weaving.
 
 At the end of the process, the vehicle is able to drive autonomously around the track without leaving the road.
 
 ####2. Final Model Architecture
 
-The final model architecture (model.py lines 18-24) consisted of a convolution neural network with the following layers and layer sizes ...
+The final model architecture (driving_nn.py lines 122-135) consisted of a convolution neural network with the following layers and layer sizes 
 
-Here is a visualization of the architecture (note: visualizing the architecture is optional according to the project rubric)
-
-![alt text][image1]
+Lambda - Normalization layer (x/255/- 0.5)
+Cropping2D - Remove the sky from consideration
+Convolution2D - 24 5x5 filters
+MaxPooling2D - 2x2 pooling
+Dropout - keep 70% of the values
+Flatten - Flatten the model out
+Dense - output the single value
 
 ####3. Creation of the Training Set & Training Process
 
-To capture good driving behavior, I first recorded two laps on track one using center lane driving. Here is an example image of center lane driving:
+I used the sample data for this, as my personal data is not working as well
 
-![alt text][image2]
+To augment the data set, I also flipped images and angles thinking that this would allow me to double the amount of data such that if one set of data trains on the right, the flipped images would train on the left and vice versa. I also filtered the data before flipping it by removing about 98% of the points with center steering values between -0.1 and 0.1.
 
-I then recorded the vehicle recovering from the left side and right sides of the road back to center so that the vehicle would learn to .... These images show what a recovery looks like starting from ... :
+After the collection process, I had 24000 number of data points. After augmentation I have 10000 which includes right and left steering values.
 
-![alt text][image3]
-![alt text][image4]
-![alt text][image5]
+I finally randomly shuffled the data set and put 20% of the data into a validation set. 
 
-Then I repeated this process on track two in order to get more data points.
-
-To augment the data sat, I also flipped images and angles thinking that this would ... For example, here is an image that has then been flipped:
-
-![alt text][image6]
-![alt text][image7]
-
-Etc ....
-
-After the collection process, I had X number of data points. I then preprocessed this data by ...
-
-
-I finally randomly shuffled the data set and put Y% of the data into a validation set. 
-
-I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was Z as evidenced by ... I used an adam optimizer so that manually training the learning rate wasn't necessary.
+I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was 3 as evidenced by the validation mse increasing after that. I used an adam optimizer so that manually training the learning rate wasn't necessary
